7c0f11efeaaa75723af5d218bf20618a6925e235
Adrián Pardellas Blunier <adrian@anavallasuiza.com>
2016-06-15 14:20:25 +0200

    added es and gl trans files

---
+++ lang/gl/messages.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'file_browser_title'                   => '',
+    'create_directory'                     => '',
+    'upload_file'                          => '',
+    'back_button'                          => '',
+    'cancel_button'                        => '',
+    'confirm_button'                       => '',
+    'modal_create_directory'               => '',
+    'modal_create_directory_input_label'   => '',
+    'modal_upload_file'                    => '',
+    'modal_upload_file_input_label'        => '',
+    'modal_delete_title'                   => '',
+    'modal_delete_text'                    => '',
+    'alert_success_create_directory_title' => '',
+    'alert_success_create_directory_text'  => '',
+    'alert_success_destroy_dir_title'      => '',
+    'alert_success_destroy_dir_text'       => '',
+    'alert_success_upload_file_title'      => '',
+    'alert_success_upload_file_text'       => '',
+    'alert_success_destroy_file_title'     => '',
+    'alert_success_destroy_file_text'      => '',
+];
