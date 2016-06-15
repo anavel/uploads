fa60bb1872f4108cced0b105e27cd6f2830079fe
Adrián Pardellas Blunier <adrian@anavallasuiza.com>
2016-06-15 14:11:55 +0200

    added english translation messages

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -1,5 +1,24 @@
 <?php
 
 return [
-
+    'file_browser_title'                   => 'File Browser',
+    'create_directory'                     => 'New folder',
+    'upload_file'                          => 'Upload file',
+    'back_button'                          => 'Back',
+    'cancel_button'                        => 'Cancel',
+    'confirm_button'                       => 'Confirm',
+    'modal_create_directory'               => 'Create folder',
+    'modal_create_directory_input_label'   => 'Name',
+    'modal_upload_file'                    => 'Upload file',
+    'modal_upload_file_input_label'        => 'File',
+    'modal_delete_title'                   => 'Delete',
+    'modal_delete_text'                    => 'This action cannot be undone! Are you sure?',
+    'alert_success_create_directory_title' => 'Folder created',
+    'alert_success_create_directory_text'  => 'The folder was created successfully!',
+    'alert_success_destroy_dir_title'      => 'Folder removed',
+    'alert_success_destroy_dir_text'       => 'The folder was removed successfully!',
+    'alert_success_upload_file_title'      => 'File uploaded',
+    'alert_success_upload_file_text'       => 'The file was uploaded successfully!',
+    'alert_success_destroy_file_title'     => 'File removed',
+    'alert_success_destroy_file_text'      => 'The file was removed successfully!',
 ];
